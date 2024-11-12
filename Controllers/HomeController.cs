604b543c0bf4ebb0b3cf24fcb817197020da621b
B0maAp1r1 <337405@edu.mkcollege.ac.uk>
2024-11-12 11:51:39 +0000

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Gibjohn_Tutoring.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace Gibjohn_Tutoring.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
