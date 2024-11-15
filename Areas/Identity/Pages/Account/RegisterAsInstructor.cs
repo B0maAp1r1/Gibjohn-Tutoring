6d9e79274e45ad102bb69f1989544080b1f82cca
B0maAp1r1 <337405@edu.mkcollege.ac.uk>
2024-11-15 11:36:46 +0000

    commit

---
+++ Areas/Identity/Pages/Account/RegisterAsInstructor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gibjohn_Tutoring.Areas.Identity.Pages.Account
+{
+    public class RegisterAsInstructor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
