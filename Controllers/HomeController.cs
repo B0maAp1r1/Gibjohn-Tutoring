907bc1ba4cf58b884d5970773ba2c8c53f59ac9a
B0maAp1r1 <337405@edu.mkcollege.ac.uk>
2024-11-22 12:30:50 +0000

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,16 @@ namespace Gibjohn_Tutoring.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
